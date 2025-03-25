05f257fa113b4aba44d59754e8cd296967931fd1
Marieezouaa <88006867+Marieezouaa@users.noreply.github.com>
2025-03-25 15:50:54 -0400

    initial files

---
+++ common.php
@@ -0,0 +1,16 @@
+<!---This is for functions that are common in other files-->
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
