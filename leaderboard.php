0b06a4f34b6e6c4b9e48c12c8e2830b0eb6bedc6
Victoria Nwanze <vickyonwa@gmail.com>
2025-04-07 00:10:24 -0400

    adds leaderboard logic and style

--- leaderboard.php
+++ leaderboard.php
@@ -1,9 +1,27 @@
 <!--We can use this page for the leaderboard logic-->
 <?php
-session_start();
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /* if (isset($_COOKIE['name']) && (isset($_COOKIE['earnings']))) {
+        $name = $_COOKIE['name'];
+        $earnings = $_COOKIE['earnings'];
         
+        $file = 'leaderboard.txt';
+        $leaderboardEntry = "$name, $earnings\n";
+        file_put_contents($file, $leaderboardEntry, FILE_APPEND);
+    } */
+    $file = 'leaderboard.txt';
+    if (file_exists($file)) {
+        $data = file_get_contents($file);
+        $lines = explode("\n", $data);
+        $leaderboard = [];
+
+        for ($i = 0; $i < count($lines); $i++) {
+            list($name, $earnings) = explode(",", $lines[$i]);
+            $leaderboard[] = ['name' => $name, 'earnings' => (int)$earnings];
+        }
+        
+        usort($leaderboard, function($a, $b){
+            return $b['earnings'] - $a['earnings'];            
+        });
     }
 ?>
 
@@ -12,9 +30,38 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Leaderboard</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <div id="main-content">
+        <h1 style="font-size: 30pt; margin: 1% 0 0 0;">Leaderboard</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Earnings</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    for ($i = 0; $i < min(count($leaderboard),10); $i++) {
+                        echo "<tr>";
+                        echo "<td>" . ($i + 1) . "</td>";
+                        echo "<td>" . $leaderboard[$i]['name'] . "</td>";
+                        echo "<td>" . $leaderboard[$i]['earnings'] . "</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+        <div id="bottom">
+            <a href="signup.php">
+                <button id="home"><img src="assets/images/home.png"></button>
+            </a>
+        </div>
+    </div>
     
 </body>
 </html>
