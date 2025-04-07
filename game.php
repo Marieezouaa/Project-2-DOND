f6e81fe1ed271624fae23b3146425e3ee8c4c298
Victoria Nwanze <vickyonwa@gmail.com>
2025-04-07 19:57:59 -0400

    turns cases into clickable buttons

--- game.php
+++ game.php
@@ -1,7 +1,10 @@
 <!--User will be brought here from the welcome page-->
 <?php
+    session_start();
+    include 'game_logic.php';
+
     $prices = [.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000,
-            50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];      
+            50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +17,6 @@
 </head>
 
 <body>
-    <img src="assets/images/brief-case.png" alt="">
 
     <div id="main-content">
         <p> Deal or No Deal</p>
@@ -34,42 +36,50 @@
 
             <div id="case-area">
 
-                       <?php
-
-                for ($i = 0; $i < 3; $i++){
-                        echo '<div class="price-container"> 
-                         <img src="assets/images/brief-case.png" alt="">
-                         <img src="assets/images/brief-case.png" alt="">
-                         <img src="assets/images/brief-case.png" alt="">
-                         <img src="assets/images/brief-case.png" alt="">
-                         <img src="assets/images/brief-case.png" alt="">
-                        </div>';
+                <?php
+                    $caseNum = 1;
+                    for ($i = 0; $i < 6; $i++) {
+                        if ($caseNum == 26) {
+                            echo '<div class="price-container" style="padding: 1%">';
+                            echo '<form method="POST" id="form">';
+                            echo '<button type="submit" class="case-button">';
+                            echo '<div class="case-and-number">';
+                            echo '<img src="assets/images/brief-case.png" class="cases" alt="" style="width: 15%; height: 15%;">';
+                            echo '<div class="case-number">' . $caseNum . '</div>';
+                            echo '</div>';
+                            echo '</button>';
+                            echo '</form>';
+                            echo '</div>';
+                            break;
+                        }
+                        echo '<div class="price-container" style="padding: 1%">';
+                        for ($j = 0; $j < 5; $j++) {
+                            echo '<form method="POST" id="form">';
+                            echo '<button type="submit" class="case-button">';
+                            echo '<div class="case-and-number">';
+                            echo '<img src="assets/images/brief-case.png" class="cases" alt="">';
+                            echo '<div class="case-number">' . $caseNum . '</div>';
+                            echo '</div>';
+                            echo '</button>';
+                            echo '</form>';
+                            $caseNum++;
+                        }
+                        echo '</div>';
                     }
-
-                ?>  
-
+                ?> 
+                <div id="bottom-case-area">
+                    <img src="assets/images/brief-case.png" class="player-case" alt="" style="width: 15%; height: 15%; margin-left: 5%;">
+                </div> 
             </div>
 
             <div id="right-prices">
-
                 <?php
-
                 for ($i = 13; $i < 26; $i++){
                         echo '<div class="price-container">' . "$" . number_format($prices[$i]) . '</div>';
                     }
-
                 ?>
-
             </div>
-
-
         </div>
     </div>
-
-
-
-
-
 </body>
-
 </html>
